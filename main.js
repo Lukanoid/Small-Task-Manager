0de4238c83fa9e2c199ff703885228d114429b88
Vasil Stamboliiski <vasil.stamboliiski1@gmail.com>
2026-03-10 08:53:11 +0200

    Addded funtionality for showtasks

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 let tasks = [];
 let id = 1;
 
+let showTasks = function(){
+
+    for(let i = 0; i < tasks.length; i++){
+        if(tasks[i].completion === "false"){
+            console.log(`[] ${tasks[i]}`)
+        }
+        else{
+            console.log(`[x] ${tasks[i].title}`)
+        }
+    }
+
+}
+
 //addTask Functionality (id generator aint working as i wanted)
 let addTask = function(title){
     let currentTask = {
@@ -11,8 +24,11 @@ let addTask = function(title){
     id++;
 
     tasks.push(currentTask)
+    console.log(`Task: ${title} successfully added.`)
 }
 
 addTask("Gym")
 addTask("Study")
+
+showTasks()
 

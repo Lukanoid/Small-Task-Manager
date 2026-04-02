0a7ea0c045560280027b320d2170db3d66a1d583
Vasil Stamboliiski <vasil.stamboliiski1@gmail.com>
2026-04-03 00:57:43 +0300

    Added more information in case of unknown command

--- main.js
+++ main.js
@@ -1,31 +1,41 @@
 const {
-  addTask,
-  deleteTask,
-  editTask,
-  completeTask,
-  showTasks,
-  showCompletedTasks,
-  showPendingTasks,
+    addTask,
+    deleteTask,
+    editTask,
+    completeTask,
+    showTasks,
+    showCompletedTasks,
+    showPendingTasks,
 } = require("./taskManager");
 
 const command = process.argv[2];
 const arg1 = process.argv[3];
 const arg2 = process.argv[4];
 
-if(command === "add"){
+if (command === "add") {
     addTask(arg1);
-}else if(command === "list"){
+} else if (command === "list") {
     showTasks();
-}else if(command === "complete"){
+} else if (command === "complete") {
     completeTask(Number(arg1))
-}else if(command === "edit"){
+} else if (command === "edit") {
     editTask(Number(arg1), arg2)
-}else if(command === "delete"){
+} else if (command === "delete") {
     deleteTask(Number(arg1))
-}else if(command === "completed"){
+} else if (command === "completed") {
     showCompletedTasks()
-}else if (command === "pending"){
+} else if (command === "pending") {
     showPendingTasks()
-}else{
+} else {
     console.log("Unknown command.")
+    console.log(`
+  Available commands:
+  node main.js add "Task title" - for adding task.
+  node main.js list - for seeing all tasks.
+  node main.js complete 1 - for completing task.
+  node main.js edit 1 "New title" - for editing task.
+  node main.js delete 1 - for deleting task.
+  node main.js completed - for seeing all completed tasks.
+  node main.js pending - for seeing all uncompleted tasks.
+`);
 }

129d023c069748ec281031caea578c68690b0135
Vasil Stamboliiski <vasil.stamboliiski1@gmail.com>
2026-04-02 17:38:05 +0300

    Added functionality for accepting commands from terminal

--- main.js
+++ main.js
@@ -5,17 +5,27 @@ const {
   completeTask,
   showTasks,
   showCompletedTasks,
-  showPendingTasks
+  showPendingTasks,
 } = require("./taskManager");
 
-addTask("Gym");
-addTask("Sports");
-addTask("Cars");
+const command = process.argv[2];
+const arg1 = process.argv[3];
+const arg2 = process.argv[4];
 
-showTasks();
-
-completeTask(1);
-editTask(2, "Football");
-
-showCompletedTasks();
-showPendingTasks();
+if(command === "add"){
+    addTask(arg1);
+}else if(command === "list"){
+    showTasks();
+}else if(command === "complete"){
+    completeTask(Number(arg1))
+}else if(command === "edit"){
+    editTask(Number(arg1), arg2)
+}else if(command === "delete"){
+    deleteTask(Number(arg1))
+}else if(command === "completed"){
+    showCompletedTasks()
+}else if (command === "pending"){
+    showPendingTasks()
+}else{
+    console.log("Unknown command.")
+}

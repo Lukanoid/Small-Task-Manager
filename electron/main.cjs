0a03ff5368a9e0bca6228edd5af5991ce64422a1
Vasil Stamboliiski <vasil.stamboliiski1@gmail.com>
2026-04-12 06:59:18 +0300

    Added main.cjs so it works with electron

---
+++ electron/main.cjs
@@ -0,0 +1,51 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+const taskManager = require("../taskManager");
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 900,
+        height: 700,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.cjs"),
+            contextIsolation: true,
+            nodeIntegration: false
+        }
+    });
+
+    win.loadFile(path.join(__dirname, "../ui/index.html"));
+}
+
+app.whenReady().then(() => {
+    ipcMain.handle("tasks:getAll", async () => {
+        return taskManager.getAllTasks();
+    });
+
+    ipcMain.handle("tasks:add", async (_, title, priority) => {
+        return taskManager.addTask(title, priority);
+    });
+
+    ipcMain.handle("tasks:delete", async (_, id) => {
+        return taskManager.deleteTask(id);
+    });
+
+    ipcMain.handle("tasks:toggle", async (_, id) => {
+        return taskManager.toggleTask(id);
+    });
+
+    ipcMain.handle("tasks:edit", async (_, id, newTitle, newPriority) => {
+        return taskManager.editTask(id, newTitle, newPriority);
+    });
+
+    ipcMain.handle("tasks:clearAll", async () => {
+        return taskManager.clearAllTasks();
+    });
+
+    createWindow();
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
